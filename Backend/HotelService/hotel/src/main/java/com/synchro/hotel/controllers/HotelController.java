e38f1f037c41a523f91d27db5f0e9721fd7525c5
douglasliralima <douglasliralima@gmail.com>
2020-03-12 14:33:51 -0300

    0.2.4v - java don't saved files

--- Backend/HotelService/hotel/src/main/java/com/synchro/hotel/controllers/HotelController.java
+++ Backend/HotelService/hotel/src/main/java/com/synchro/hotel/controllers/HotelController.java
@@ -95,4 +95,6 @@ public class HotelController {
 
         return mapHotels;
     }
+
+    
 }
---
+++ Backend/HotelService/hotel/src/main/java/com/synchro/hotel/models/Services.java
@@ -0,0 +1,11 @@
+package com.synchro.hotel.models;
+
+public class Services{
+    public Integer pool;
+    public Integer parking;
+    public Integer breakfast;
+    public Integer pool;
+    public Integer pool;
+    public Integer pool;
+
+}
